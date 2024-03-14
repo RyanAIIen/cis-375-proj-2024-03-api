bc0c8f3d8908bd8919c8b8275dccb36f7a5c4c76
Ryan Allen <allenrya@umich.edu>
2024-03-13 23:10:49 -0400

    initial project commit

--- example/views.py
+++ example/views.py
@@ -1,16 +1,18 @@
-# example/views.py
 from datetime import datetime
 
 from django.http import HttpResponse
 
 def index(request):
-    now = datetime.now()
-    html = f'''
+    return HttpResponse('''
     <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
+        <head>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        </head>
+        <body class="bg-warning-subtle">
+            <div class="container" style="text-align: center; margin-top: 10rem;">
+                <h1>CIS-375 Hotel Management API</h1>
+                <p>Coming Soon.</p>
+            </div>
         </body>
     </html>
-    '''
-    return HttpResponse(html)
+    ''')
