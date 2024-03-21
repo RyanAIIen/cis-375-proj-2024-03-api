3d5e81cabe1aeb18a2309c8bd0ab9165b31f3438
Ryan Allen <allenrya@umich.edu>
2024-03-21 01:39:03 -0400

    ...

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@
 
 pip install -r requirements.txt
 
-python3 manage.py migrate
-python3 manage.py collectstatic --no-input
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic --no-input
--- main/settings.py
+++ main/settings.py
@@ -133,9 +133,8 @@ USE_TZ = True
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'main/static/'),)
 
-STATIC_DIRNAME = 'static'
-STATIC_URL = f'/{STATIC_DIRNAME}/'
-STATIC_ROOT = os.path.join(BASE_DIR, f'.{STATIC_DIRNAME}')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
